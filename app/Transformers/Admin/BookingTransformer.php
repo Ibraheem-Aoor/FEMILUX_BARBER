b9cecb30f36781a72d150b65f6cd25396a2a635c
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-07-03 07:24:31 +0300

    fix delete route in bookings

--- app/Transformers/Admin/BookingTransformer.php
+++ app/Transformers/Admin/BookingTransformer.php
@@ -37,7 +37,7 @@ class BookingTransformer extends TransformerAbstract
             data-bs-target="#booking-modal" data-services="' . $services. '" data-header-title="' . __('general.booking_services_with_barber' , ['barber' => $booking->barber->name]).'"
             class="btn btn-sm btn-soft-primary"><img loading="lazy" width="10" height="10" src="' . asset('assets/user/libs/feather-icons/icons/eye-line.svg') . '"></a>
 
-        <a data-bs-toggle="modal" data-bs-target="#delete-modal" data-delete-url="' . route('admin.service.destroy', encrypt($booking->id)) . '"
+        <a data-bs-toggle="modal" data-bs-target="#delete-modal" data-delete-url="' . route('admin.booking.destroy', encrypt($booking->id)) . '"
         data-message="' . __('general.confirm_delete') . '" data-name="' . $booking->name . '" id="row-' . $booking->id . '"
         href="#" class="btn btn-sm btn-danger ms-2"><img loading="lazy" width="10" height="10" src="' . asset('assets/user/libs/feather-icons/icons/trash.svg') . '"><i class="fa fa-eye"></i></a>
         </div>';
